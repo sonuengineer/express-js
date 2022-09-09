3c9c68285a9fd327053df5443223044d30c27f9e
sonuengineer <prajapatisonu50@gmail.com>
2022-09-09 13:08:26 +0530

    router learning in express

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-const http = require('http');
-const express=require('express');
-const app=express();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.use((req,res,next)=>{
-    console.log("in middileware");
-    next();
-})
+const app = express();
 
-app.use((req,res,next)=>{
-    console.log("next middileware");
-    res.send("<h1>express learning in easy way</h1>")
-})
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title">Add Product<br><input type="text" name="size">Add size of the Product<br><button type="submit">submit</button></form>');
+  //res.send('<form action="/product" method="POST"><input type="text" name="size"><button type="submit">Add size of the Product</button></form>');
+
+});
+
+app.post('/product', (req, res, next) => {
+    
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+
+
 
 //const server=http.createServer(app)
 //server.listen(3000)
